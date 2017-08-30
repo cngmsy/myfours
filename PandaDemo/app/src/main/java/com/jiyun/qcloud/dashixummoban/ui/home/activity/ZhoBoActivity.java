6703849872d12c22c92cb6b80184fd9de577cb25
lwp666 <482961603@qq.com>
2017-08-30 19:00:00 +0800

    修改

--- PandaDemo/app/src/main/java/com/jiyun/qcloud/dashixummoban/ui/home/activity/ZhoBoActivity.java
+++ PandaDemo/app/src/main/java/com/jiyun/qcloud/dashixummoban/ui/home/activity/ZhoBoActivity.java
@@ -3,6 +3,10 @@ package com.jiyun.qcloud.dashixummoban.ui.home.activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.os.Build;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jiyun.qcloud.dashixummoban.R;
@@ -22,13 +26,16 @@ public class ZhoBoActivity extends BaseActivity implements ZhiContract.View{
     private ZhiContract.Presenter presenter;
     private String title;
     private VideoView myview;
+    private TextView titletext;
+    private ImageView backimage;
     @Override
     protected void initData() {
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
         title = intent.getStringExtra("title");
             presenter.seconed(url);
-
+        titletext.setText(title);
+        Toast.makeText(this, title, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -36,6 +43,25 @@ public class ZhoBoActivity extends BaseActivity implements ZhiContract.View{
         new ZhiPresenter(this);
         Vitamio.isInitialized(getContext());
         myview = (VideoView) findViewById(R.id.zhiboview);
+        titletext = (TextView) findViewById(R.id.titletext);
+        backimage = (ImageView) findViewById(R.id.back_image);
+        backimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+//        if(View.VISIBLE==backimage.getVisibility())
+//        {
+//            backimage.setVisibility(View.GONE);
+//            titletext.setVisibility(View.GONE);
+//        }else {
+//            if(View.VISIBLE!=backimage.getVisibility())
+//            {
+//                backimage.setVisibility(View.VISIBLE);
+//        	/*处理其他逻辑*/
+//            }
+//        }
 
     }
 
@@ -68,9 +94,10 @@ public class ZhoBoActivity extends BaseActivity implements ZhiContract.View{
     public void showVideoList(XiuChang xiuChang) {
         XiuChang.FlvUrlBean flv_url = xiuChang.getFlv_url();
         String flv1 = flv_url.getFlv2();
-        Toast.makeText(this, flv1+title, Toast.LENGTH_SHORT).show();
         myview.setVideoPath(flv1);
         myview.start();
+
+
     }
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -83,6 +110,22 @@ public class ZhoBoActivity extends BaseActivity implements ZhiContract.View{
         if(getRequestedOrientation()!= ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
+    }
+    //真正的沉浸式模式
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
     }
 
     @Override
